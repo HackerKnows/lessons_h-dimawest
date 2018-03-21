fad18649d0b54e3c082c14ce05e71e5297422ac5
dimawest <dimawest198422@gmail.com>
2018-03-21 20:25:08 +0300

    плотность,сколько прошло недель

---
+++ 03. ifs/ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("количество жителей");
+            int amount = int.Parse(Console.ReadLine());
+            Console.WriteLine (amount);
+            Console.WriteLine("введите его площадь");
+            int area= int.Parse (Console.ReadLine());
+            Console.WriteLine("плотность="+amount / area);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03. ifs/ConsoleApp9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("количество дней прошедших с начала года");
+            int days = int.Parse(Console.ReadLine());
+            Console.WriteLine("прошло недель"+ days / 7);
+            Console.ReadLine();
+        }
+    }
+}
