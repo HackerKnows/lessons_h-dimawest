65aa7ab2469ce7535dcc5c69c9217f3102f9aa56
dimawest <dimawest198422@gmail.com>
2018-03-28 20:07:02 +0300

    i'm dima

---
+++ 03. ifs/апельсины между людьми/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace апельсины_между_людьми
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите количество апельсинов");
+            int orange = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите кол - во людей");
+            int people = int.Parse(Console.ReadLine());
+            Console.WriteLine(orange / people);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03. ifs/стоймость килограмма/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace стоймость_килограмма
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("стоймость килограмма конфет");
+            Console.WriteLine("сколько хотите купить килограмм");
+            Console.ReadLine();
+        }
+    }
+}
