f25319ee2f44b5b2c23204222b4b565a872434da
dimawest <dimawest198422@gmail.com>
2018-03-28 20:23:14 +0300

    наоборот

---
+++ 03. ifs/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите первое слово");
+            string word = Console.ReadLine();
+            Console.WriteLine("введите второе слово");
+            string word2 = Console.ReadLine();
+            Console.WriteLine("введите третье слово");
+            string word3 = Console.ReadLine();
+            Console.WriteLine( word3+" "+ word2+" "+ word+" ");
+            Console.ReadLine();
+        }
+    }
+}
