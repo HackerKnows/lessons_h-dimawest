c9871acadd8267b26db159a00ca2e4133ff18631
dimawest <dimawest198422@gmail.com>
2018-04-04 19:41:28 +0300

    сложно

--- 06/стороны треугольника/Program.cs
+++ 06/стороны треугольника/Program.cs
@@ -10,7 +10,19 @@ namespace стороны_треугольника
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("введите сторону а треугольника");
+            Console.WriteLine("введите сторону А ");
+            int sideA = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите сторону Б ");
+            int sideB = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите сторону С ");
+            int sideC = int.Parse(Console.ReadLine());
+            if (sideA * sideA + sideB * sideB == sideC * sideC)
+            {
+                Console.WriteLine("треугольник прямоугольный"); 
+            }
+          else  {
+                Console.WriteLine("треугольник не прямоугольный");
+            }
             Console.ReadLine();
         }
     }
