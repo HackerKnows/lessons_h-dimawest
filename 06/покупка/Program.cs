27e86a872ea1c9399750ed1a0e9ba02399ae6758
dimawest <dimawest198422@gmail.com>
2018-04-04 20:13:34 +0300

    не сложно

---
+++ 06/покупка/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace покупка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите свой баланс");
+            int balance = int.Parse(Console.ReadLine());
+            if (balance > 3000)
+            {
+                Console.WriteLine("баланса хватает");
+            }
+            else
+            {
+                Console.WriteLine("не хватает денег на балансе");
+            }
+            Console.ReadLine();
+        }
+    }
+}
