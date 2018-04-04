e9af1382733ab0a6ea7ef4f5c5e3ed2f8ecd0d6c
dimawest <dimawest198422@gmail.com>
2018-04-04 19:00:53 +0300

    стоимость

---
+++ 06/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите стоимость монитора");
+            int cost = int.Parse(Console.ReadLine());
+            Console.WriteLine("стоимость системного блока");
+            int systemunit = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите стоимость клавиатуры и мыши");
+            int keyboardmouse = int.Parse(Console.ReadLine());
+            Console.WriteLine(cost + systemunit + keyboardmouse);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 06/стороны треугольника/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace стороны_треугольника
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите сторону а треугольника");
+            Console.ReadLine();
+        }
+    }
+}
