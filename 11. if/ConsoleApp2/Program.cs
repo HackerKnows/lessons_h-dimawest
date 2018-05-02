3fd5dc246227580a57fa53c19baa637fdabadf76
dimawest <dimawest198422@gmail.com>
2018-05-02 20:14:03 +0300

    МАТЧ

---
+++ 11. if/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите кол - во очков полученных за матч");
+            int score = int.Parse(Console.ReadLine());
+            if (score == 3)
+            {
+                Console.WriteLine("выигрыш");
+            }
+            else if (score == 0)
+            {
+                Console.WriteLine("проигрыш");
+            }
+            else
+            {
+                Console.WriteLine("ничья");
+            }
+            Console.ReadLine();
+        }
+    }
+}
