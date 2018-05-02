81fe9dbe6182319738823b598e91fb2ec5a23de9
dimawest <dimawest198422@gmail.com>
2018-05-02 20:06:29 +0300

    5

---
+++ 11. if/11(2)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите первое число");
+            double numberOne = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите второе число");
+            double numberTwo = double.Parse(Console.ReadLine());
+            if (numberOne > 0 && numberTwo > 0 && numberOne > numberTwo)
+            {
+                Console.WriteLine(numberOne / 2);
+            }
+            else if (numberOne < 0 && numberTwo < 0 && numberTwo > numberOne)
+            {
+                Console.WriteLine(numberOne/2);
+            }
+            else
+            {
+                Console.WriteLine(numberOne);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11. if/11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+            int number = int.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine(number * -1);
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
