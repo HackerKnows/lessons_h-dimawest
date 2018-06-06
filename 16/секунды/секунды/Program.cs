b7ab4c5bfee1d6ac60baba3430cd1c38456bce16
Den4ik <danvoron3@gmail.com>
2018-06-06 20:18:31 +0300

    red

---
+++ 16/секунды/секунды/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace секунды
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds = 1;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(seconds);
+                seconds = 1 + seconds;
+            }
+        }
+    }
+}
